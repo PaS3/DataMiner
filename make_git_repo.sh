f7fdbaa6b085e44e9604fa3ef40de62c704a641a
paul <paul@localhost.localdomain>
2019-09-08 14:17:53 -0500

    add some automation

---
+++ make_git_repo.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo -e '''# DataMiner
+
+file: Account Intelligence_ Python Dev Mini-Project - August 2019.txt
+
+The following is an ​IMDB movie data-set​ available via Kaggle https://www.kaggle.com/carolzhangdc/imdb-5000-movie-dataset/data
+
+The column names in this data-set should be more or less self-descriptive. Some of the key columns that pertain to this mini-project are: ‘actor_1_name’, ‘actor_2_name’, ‘actor_3_name’, ‘director_name’, ‘budget’ and ‘gross’. You can make assumptions about these names as you see fit in case they do not seem descriptive enough.
+
+The task requires the following (Note: please use any libraries that you see fit, if applicable, for the following steps):
+
+    Import the file into a local db
+
+    Then write functions in Python to perform the following: a. Load the relevant data from the db in Step 1 into your program : b. For this data, compute the top 10 genres in decreasing order by their profitability.
+
+Note:​ You could (but are not required to) compute profitability as simply as: i. ‘gross’ - ’budget’ or ii. (‘gross’ - ‘budget’)/’budget’ iii. Anything advanced that you can think of
+
+c. Return the top 10 actors or directors in decreasing order by their profitability (use any definition you choose for profitability using the above guidance).
+
+d. Bonus questions (​Note: If you choose to do any of the bonus questions
+below, any one question is more than adequate​):
+
+  i.  Choice 1: Find the best actor, director pairs (up to 10) that have the
+      highest IMDB_ratings, if there are indeed any such pairs.
+  ii. Choice 2: Any interesting questions that you would like to work on if you
+      would (for e.g. imdb_score, actor facebook_likes
+  iii. Choice 3: Build a REST API to return an actor’s information (simple text output)
+
+    Write unit tests for your functions. ​Note: This is an important step for this project.
+
+    Commit code to a git repo (gitlab or github) and send us a link to it.
+
+    Also document your steps, libraries used and any instructions.
+
+Note: Please feel free to make assumptions as you see fit. If you are unable to finish the mini-project for any reason (for instance if there were time constraints or personal reasons why), please document the methodology you would take.
+''' >> README.md
+git init
+git add README.md
+git commit -m "First commit"
+git remote add origin https://github.com/monodrone/DataMiner.git
+git push -u origin master
+
+
+git add Movie_Data_Analysis.py 
+git commit -m "add main python commit"
+git push -u origin master
+
+git add make_git_repo.sh
+git commit -m "add some automation"
+git remote add origin https://github.com/monodrone/DataMiner.git
+git push -u origin master
